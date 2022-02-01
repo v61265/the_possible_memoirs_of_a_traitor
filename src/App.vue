ee35b6be917c0da89499590aa95d8c0c036eaf3a
YiHsuan <yihsuanlee@mirrormedia.mg>
2022-02-01 23:01:49 +0800

    feat: add basic component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="app">
-    <div class="hand"><img src="./assets/hand-up.png" /></div>
-    <div class="star"><img src="./assets/falling-star.png" /></div>
-    <div class="clock"><img src="./assets/clock.png" /></div>
-    <StartButton />
+    <div :class="{ blur: isHoverStart }">
+      <div class="hand"><img src="./assets/hand-up.png" /></div>
+      <div class="star"><img src="./assets/falling-star.png" /></div>
+      <div class="clock"><img src="./assets/clock.png" /></div>
+      <div class="background" />
+    </div>
+    <StartButton
+      @mouseover="handleHOverStart(true)"
+      @mouseleave="handleHOverStart(false)"
+    />
   </div>
 </template>
 
@@ -12,6 +18,16 @@ import StartButton from "./components/StartButton.vue";
 export default {
   name: "App",
   components: { StartButton },
+  data() {
+    return {
+      isHoverStart: false,
+    };
+  },
+  methods: {
+    handleHOverStart(boolean) {
+      this.isHoverStart = boolean;
+    },
+  },
 };
 </script>
 
@@ -20,8 +36,23 @@ export default {
 #app {
   width: 100vw;
   height: 100vh;
-  background: #dceeef;
+
   position: relative;
+
+  .blur {
+    filter: blur(1px);
+    * {
+      filter: blur(1px);
+    }
+  }
+
+  .background {
+    height: 102vh;
+    width: 102vw;
+    transform: translate(-1vw, -1vh);
+    background: url("./assets/background.png");
+    background-size: cover;
+  }
 
   .hand {
     width: 78px;
@@ -31,6 +62,7 @@ export default {
     right: 24px;
     background: #000000;
     transition: height 1s ease 0s, width 1s ease 0s;
+    z-index: 2;
     &:hover {
       width: calc(100vw - 160px);
       height: calc(100vh - 160px);
@@ -51,6 +83,7 @@ export default {
     left: 24px;
     background: #000000;
     transition: height 1s ease 0s, width 1s ease 0s;
+    z-index: 2;
     &:hover {
       width: calc(100vw - 160px);
       height: calc(100vh - 160px);
@@ -71,6 +104,7 @@ export default {
     right: 24px;
     background: #000000;
     transition: height 1s ease 0s, width 1s ease 0s;
+    z-index: 2;
     &:hover {
       width: 50vw;
       height: 50vh;
--- src/components/StartButton.vue
+++ src/components/StartButton.vue
@@ -1,24 +1,45 @@
 <template>
-  <div class="start">START</div>
+  <div
+    class="start"
+    @mouseover="handleMouseOver"
+    @mouseleave="handleMouseLeave"
+  >
+    START
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    handleMouseOver() {
+      this.$emit("mouseover");
+    },
+    handleMouseLeave() {
+      this.$emit("mouseleave");
+    },
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 @import url("http://fonts.cdnfonts.com/css/racing-sans-one");
 
 .start {
   font-family: Racing Sans One;
-  font-size: 48px;
-  line-height: 60px;
-  color: #000000;
-  border: 10px solid #000000;
+  font-size: 36px;
+  line-height: 45px;
+  color: #fff;
+  border: 7px solid #fff;
   box-sizing: border-box;
   border-radius: 30px;
   width: fit-content;
-  padding: 20px 30px;
+  padding: 5px 30px;
   position: absolute;
-  top: 50vh;
+  top: 70vh;
   left: 50vw;
   transform: translate(-50%, 0);
+  // z-index: 99;
+  background: rgba(255, 255, 255, 0.23);
   &:hover {
     cursor: pointer;
   }
